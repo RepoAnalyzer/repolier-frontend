91c026a7afd45e2eaed675b78d6b7016cd84f27a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-11-26 15:58:07 +0300

    style(components): removed remnant component

--- src/components/KeyboardLayout/keyToDisplayKey.tests.ts
+++
@@ -1,9 +0,0 @@
-import {describe, expect, test} from '@jest/globals';
-
-import { keyToDisplayKey } from './KeyboardLayout.utils';
-
-describe('After conversion keys are compatible with react-simple-keyboard', () => {
-  test('Simple keys are unchanged', () => {
-    expect(keyToDisplayKey('a')).toBe('a');
-  });
-});
