8cb9d9ab371b68fd170d4f35edcfdaeb35fbdd3a
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2024-06-03 22:12:38 +0300

    chore: removed unused template project remnants

--- src/api/counter/counter.service.ts
+++
@@ -1,8 +0,0 @@
-import { ICounterApi } from './counter.routes';
-import { fetchCount } from './fetchCount';
-
-class CounterService implements ICounterApi {
-    getCount = (data = 1) => fetchCount(data);
-}
-
-export const counterService = new CounterService();
