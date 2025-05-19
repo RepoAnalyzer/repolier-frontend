fb4dc6cbed59f9ef6ed058e8583fef18c796d707
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2025-05-19 20:43:08 +0300

    style: adjusted max and min values of a pull-request score

--- src/components/pull-requests/get-pull-request-score.util.ts
+++ src/components/pull-requests/get-pull-request-score.util.ts
@@ -3,11 +3,11 @@ import { RepoPullRequest } from "api/pull-requests.mapper.types";
 import { randomInRange } from "utils/random";
 
 export const prepareScoreFunction = (pullRequests: RepoPullRequest[]) => {
-    const MAX = 1.00
-    const MIN = 0.00
+    const MAX = 0.90
+    const MIN = 0.50
 
     const len = pullRequests.length
     const delta = (MAX - MIN) / len
 
-    return (pullRequest: RepoPullRequest, index: number) => (len - index) * delta - randomInRange(0, delta)
+    return (pullRequest: RepoPullRequest, index: number) => MIN + (len - index) * delta - randomInRange(0, delta)
 }
